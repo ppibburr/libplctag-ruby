ec7ba9ceefb05acea1715250bf0e9be56d4ef32b
ppibburr <tulnor33@gmail.com>
2026-07-16 08:00:16 -0400

    foo

--- lib/plc/tag.rb
+++ lib/plc/tag.rb
@@ -6,11 +6,70 @@ loader = GObjectIntrospection::Loader.new(PlcTag)
 loader.load "PlcTag"
 
 class Plc
-  class Tag < PlcTag::Tag
-    def self.new *o
+  Tag = PlcTag::Tag
+  
+  class PlcTag::Tag
+    attr_accessor :value_type
+    
+    include Enumerable
+  
+    def self.new *o, value_type: nil, &b
       p @path = o[0]
-      create *o
+      t = create *o
+      t.value_type = value_type
+      
+      if b
+        t.read 50
+        b.call t
+        t.destroy
+      end
+      
+      t
     end
+    
+    def [] i
+      raise unless value_type
+      
+      if i.is_a?(Range)
+        last = i.last
+        last = length+i.last if i.last < 0
+        
+        return((i.first..(last)).map do |i|
+          self[i]
+        end)
+      end
+      
+      send(:"get_#{value_type}", i*elem_size)
+    end
+    
+    def []= i,v
+      raise unless value_type
+      
+      send(:"set_#{value_type}", i*elem_size, v)
+      return v
+    end    
+    
+    def length
+      get_int_attribute("elem_count", 0)
+    end
+    
+    def elem_size
+      get_int_attribute("elem_size", 0)
+    end
+    
+    def map o=0, type: @value_type, &b
+      (0..(length-1)).map do |i|
+        if !type
+          b.call i*elem_size, self
+        else
+          b.call send(:"get_#{type}", i*elem_size)
+        end
+      end
+    end
+    
+    def each o=0, type: @value_type, &b
+      map(o, type: type) do |a| a end.each &b
+    end 
   end
   
   def initialize addr, path: 0, port: nil, protocol: nil, cpu: nil
@@ -22,7 +81,7 @@ class Plc
     }
   end
   
-  def tag name, elem_count: nil, elem_size: nil, timeout: 50
+  def tag name, elem_count: nil, elem_size: nil, timeout: 50, value_type: nil, &b
    str = @config.merge({
       elem_count: elem_count,
       elem_size: elem_size,
@@ -30,8 +89,8 @@ class Plc
     }).find_all do |k,v|
       !!v
     end.map do |k,v| "#{k}=#{v}" end.join("&")
-
-    return self.class::Tag.new(str, timeout)
+    
+    return self.class::Tag.new(str, timeout, value_type: value_type, &b)
   end
 end
 
@@ -43,39 +102,20 @@ class Plc::LGX < Plc
 end
 
 class Plc::ModBus < Plc
-  class Tag < Plc::Tag
-    def value
-      raise ""
-    end
-    
-    def values
-      raise ""
-    end
-  end
-  
-  module Register
-    def value offset=0, timeout: 50
-      read timeout
-      get_int16 offset, timeout
-    end
-    
-    def values len, offset=0, timeout: 50
-      read timeout
-      (0..(len-1)).map do |i|
-        get_int16 i*2
-      end
-    end
-  end
-
   def initialize *o,**k
     k[:protocol] = "modbus-tcp"
     super
   end
   
-  def holding_register i, count: 1
-    t=tag "hr#{i}", elem_count: count
-    t.extend Register
-    t
+  def coils n=1, o = 0, &b
+    t = tag "co#{o}", elem_count: n, value_type: :bit, &b
+  end
+  
+  def holding_registers n=1, o=0, &b
+    t = tag "hr#{o}", elem_count: n, value_type: :int16, &b
   end
 end
 
+if __FILE__ == $0
+  
+end
--- lib/plc/webapi.rb
+++ lib/plc/webapi.rb
@@ -16,25 +16,43 @@ class Plc
       200
     end
 
-    get "/plc/:gateway/tag/:type/:tag" do
+    def tag params, &b
       g,pp = *params[:gateway].split(":")
       params[:gateway] = g
       params[:port] = pp
       
+      v = nil
+      
       plc = Plc.new(g, port: params[:port], protocol: params[:protocol], path: params[:path])
-      t   = plc.tag(params[:tag], elem_count: params[:elem_count], timeout: 50)
-      t.read 50
+      t   = plc.tag(params[:tag], elem_count: params[:elem_count].to_i, timeout: 50, value_type: params[:type].to_sym) do |t|
+        raise "" unless ["int16","int32","int8","bit","int64","uint8","uint16","uint32","uint64","float32", "float64"].index(params[:type])
+        
+        if params["value"]
+          t[0] = params['value'].to_i
+          t.write 50
+        end
+        
+        if params['values']
+          JSON.parse(params['values']).each_with_index do |val,i|
+            t[i] = val
+          end
+          t.write 50
+        end
+       
+        v = t.to_a
+      end
+      v
+    end
 
-      raise "" unless ["int16","int32","int8","bit","int64","uint8","uint16","uint32","uint64","float32", "float64"].index(params[:type])
-
-      t.send("get_#{params[:type]}",0).to_json
+    get "/plc/:gateway/tag/:type/:tag" do
+      tag(params).to_json
     end
 
     post "/plc/:gateway/tags" do
       g,pp = *params[:gateway].split(":")
       params[:gateway] = g
       params[:port] = pp
-      p g
+
       plc = Plc.new(g, port: params[:port], protocol: params[:protocol], path: params[:path])
       
       values = {}
@@ -43,19 +61,25 @@ class Plc
       tags = JSON.parse(request.body.read) rescue {}
       
       tags.each do |tag|
-        t   = plc.tag(tag["name"], elem_count: tag["count"]||=1, timeout: 50)
-        t.read 50
+        t   = plc.tag(tag["name"], elem_count: tag["count"]||=1, timeout: 50, value_type: tag["type"]) do |t|
+          raise "" unless ["int16","int32","int8","bit","int64","uint8","uint16","uint32","uint64","float32", "float64"].index(tag["type"])
 
-        raise "" unless ["int16","int32","int8","bit","int64","uint8","uint16","uint32","uint64","float32", "float64"].index(tag["type"])
-        p tag
-        if tag["value"]
-          t.send("set_#{tag["type"]}",0, tag["value"])
-          t.write 50
-          t.read 50
-          p t.send("get_#{tag["type"]}",0)
-        end
+          if tag["value"]
+            t[0] = tag["value"]
+            t.write 50
+            t.read 50
+          end
+          
+          if tag["values"]
+            tag["values"].each_with_index do |val,i|
+              t[i] = val
+            end
+            t.write 50
+            t.read 50
+          end
         
-        values[tag["name"]] = {value: t.send("get_#{tag["type"]}",0), type: tag["type"]}
+          values[tag["name"]] = {value: t[0], type: tag["type"], values: t.to_a.to_json}
+        end
       end
       
       values.to_json
